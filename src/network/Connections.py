e0a205ce1d63951ee0e5dba7a8cbd2100b1ffef6
Waldemar Schaefer <waldemar.schaefer@fh-bielefeld.de>
2023-04-25 16:17:36 +0200

    add base funktions to connection

--- src/network/Connections.py
+++ src/network/Connections.py
@@ -6,4 +6,25 @@ x =  '{ "name":"John", "age":30, "city":"New York"}'
 y = json.loads(x)
 
 # controll
-print(y["age"]) 
+print(y["age"])
+
+# Nope
+
+def send_Info():
+	# TODO ein int gibt an ob der Spieler oder gewonnen oder verloren hat oder
+	return 0
+
+
+def send_State():
+	# TODO sendet Alle KArten + Oberste Karte im Ablagestapel + Anzahl der Karten des gegenspielers
+	return 0
+
+
+def send_History():
+	# TODO sendet die gesammt Spiel History
+	return 0
+
+
+def send_Profile():
+	# TODO sendet die Bestätigung zu einem Spiel mit der Angabe des Tokens
+	return 0
