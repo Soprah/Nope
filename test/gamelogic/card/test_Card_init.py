6b3f7a30c1202645c88f3e89bb4583fda187b639
Eric <soprah@gmx.de>
2023-04-30 17:42:45 +0200

    Added .gitignore + first __init__ test for the upcoming Card class to check whether a color is valid

---
+++ test/gamelogic/card/test_Card_init.py
@@ -0,0 +1,16 @@
+import unittest
+class MyTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+        self.color = [("red"), ("blue"), ("yellow"), ("green")]
+
+# Einfarbige Karten
+
+    def test_invalid_color(self):
+        with self.assertRaises(ValueError):
+            card = Card(("purple"))
+
+
+if __name__ == '__main__':
+    unittest.main()
