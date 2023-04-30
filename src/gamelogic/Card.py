53266701cd85ad7b6215f83ef37dbc713c13a563
Eric <soprah@gmx.de>
2023-04-30 18:11:08 +0200

    Card Class: Added attribute color_amount and a test to check whether a given color is empty

--- src/gamelogic/Card.py
+++ src/gamelogic/Card.py
@@ -1,7 +1,14 @@
 class Card:
 
-    def __init__(self, color):
+    def __init__(self, color_amount, color):
         allowed_colors = ("red", "blue", "green", "yellow",)
+        if not isinstance(color, tuple):
+            color = (color,)
+
+        if len(color) != color_amount:
+            raise ValueError(f"Expected {color_amount} colors, got {len(color)}")
+
+        self.color_amount = color_amount
 
         for c in color:
             if c not in allowed_colors:
--- test/gamelogic/card/test_Card_init.py
+++ test/gamelogic/card/test_Card_init.py
@@ -10,10 +10,20 @@ class MyTestCase(unittest.TestCase):
 
 # Einfarbige Karten
 
+    # Gültige Farbe
     def test_invalid_color(self):
         with self.assertRaises(ValueError):
-            card = Card(("purple"))
+            card = Card(1, ("purple"))
 
+    # Farbe ist in einem Tupel gespeichert
+    def test_one_color_init_tuple(self):
+        card = Card(self.color[0])
+        self.assertIsInstance(card.color, tuple)
+
+    # Keinen leeren String als Farbe übergeben
+    def test_one_color_init_empty(self):
+        with self.assertRaises(ValueError):
+            card = Card(1, ())
 
 if __name__ == '__main__':
     unittest.main()
