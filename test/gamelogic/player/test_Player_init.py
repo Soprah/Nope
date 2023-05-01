e6161e03b8e6317a743468fd19b0b1f2cc1c7949
Eric <soprah@gmx.de>
2023-05-01 21:15:04 +0200

    Class Player: __init__ done with id test

---
+++ test/gamelogic/player/test_Player_init.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.gamelogic.deck.Deck import Deck
+from src.gamelogic.player.Player import Player
+
+
+class TestPlayer(unittest.TestCase):
+
+    def setUp(self):
+        self.deck = Deck()
+        self.player = Player(self.deck, "Eric")
+
+    def test_player_unique_id(self):
+        player2 = Player(self.deck, "Marc")
+        self.assertNotEqual(self.player.id, player2.id)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
