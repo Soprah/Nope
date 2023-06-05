da3b1b4b6fac53d2163e9869203026ad1bb35526
Eric <soprah@gmx.de>
2023-06-06 00:29:27 +0200

    Class SelectionCard: Added tests for to_dict_actual_card() - forgot to add the changes!

--- test/gamelogic/card/selection_card/test_SelectionCard_to_dict_actual_card.py
+++ test/gamelogic/card/selection_card/test_SelectionCard_to_dict_actual_card.py
@@ -1,8 +1,35 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from src.gamelogic.card.SelectionCard import SelectionCard
 
+
+class TestSelectionCardToDictActualCard(unittest.TestCase):
+    def setUp(self):
+        self.selection_card_single = SelectionCard(1, ("blue"))
+        self.selection_card_multiple = SelectionCard(2, (("red"), ("blue"), ("green"), ("yellow")))
+
+    # Einfarbige SelectionCard
+    def test_to_dict_actual_card_single_color(self):
+        expected_dict = {
+            "id": 1,
+            "color_amount": 1,
+            "color": ("blue",),
+            "type": "action",
+            "content": "selection",
+        }
+        actual_dict = self.selection_card_single.to_dict_actual_card()
+        self.assertDictEqual(expected_dict, actual_dict)
+
+    # Vierfarbige SelectionCard
+    def test_to_dict_actual_card_four_color(self):
+        expected_dict = {
+            "id": 2,
+            "color_amount": 4,
+            "color": (("red"), ("blue"), ("green"), ("yellow")),
+            "type": "action",
+            "content": "selection",
+        }
+        actual_dict = self.selection_card_multiple.to_dict_actual_card()
+        self.assertDictEqual(expected_dict, actual_dict)
 if __name__ == '__main__':
     unittest.main()
