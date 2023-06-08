f4a36472673ec6458a0f5b7f108f525a98532943
Eric <soprah@gmx.de>
2023-06-08 15:50:57 +0200

    Class DataConvert: Added is_data_from_active_player() and its tests

---
+++ test/gamemanagement/dataconvert/test_is_data_from_active_player.py
@@ -0,0 +1,27 @@
+import unittest
+
+from src.gamelogic.game.Game import Game
+from src.gamelogic.player.Player import Player
+from src.gamemanagement.DataConvert import DataConvert
+
+
+class TestDataConvertIsDataFromActivePlayer(unittest.TestCase):
+
+    def setUp(self):
+        self.p1 = Player("Eric", 33)
+        self.p2 = Player("Marc", 19)
+        self.game = Game(self.p1, self.p2)
+        self.dc = DataConvert()
+
+    # Correct token
+    def test_is_data_from_active_player_true(self):
+        self.assertTrue(self.dc.is_data_from_active_player(33, self.game))
+
+
+    # Incorrect token
+    def test_is_data_from_active_player_false(self):
+        self.assertFalse(self.dc.is_data_from_active_player(19, self.game))
+
+
+if __name__ == '__main__':
+    unittest.main()
