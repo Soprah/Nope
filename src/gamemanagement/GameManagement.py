9420b0fc7f586e95d21929d34127ccc573c51be5
Eric <soprah@gmx.de>
2023-06-08 20:24:22 +0200

    Class GameManagement: Added method declarations that need to be implemented

--- src/gamemanagement/GameManagement.py
+++ src/gamemanagement/GameManagement.py
@@ -14,11 +14,12 @@ class GameManagement:
                 ⇾ GM weist die empfangenen Daten der gesuchten Spielsitzung zu
     """
 
+		
+
+	"""
 	def __init__(self):
 		self.game_sessions = {}
-
-	def get_player_data(self):
-		pass
+		self.game_
 
 	def assign_player_to_game(self, p1_id, p1_name, p2_id, p2_name):
 		if p1_id in self.game_sessions or p2_id in self.game_sessions or p1_id == p2_id:
@@ -33,8 +34,28 @@ class GameManagement:
 	def get_game(self, p_id):
 		game = self.game_sessions.get(p_id)
 		return game
+	"""
 
-# Spiel beitreten
-# -> assign_player_to_game
-# -> return "You will play against {opponent.id}"
-#
+	# TODO: Empfängt Spielerdaten (name=string, token=string, room=string)
+	def receive_player_data(self, player_data):
+		pass
+
+	# TODO: Sucht einen room mit der id
+	def seek_room(self, room_id):
+		pass
+
+	# TODO: Baut einen room zum Spielen, wenn es einen room mit dem übergebenen string noch nicht gibt
+	def build_room(self, room_id):
+		pass
+
+	# TODO: Startet das Spiel
+	def start_game(self):
+		pass
+
+	# TODO: Verschickt die Spielzugdaten, welches von DataConvert bereitgestellt wurde
+	def send_turn_data(self):
+		pass
+
+	# TODO: Empfängt die Spielzugdaten, welches von Network geliefert wird
+	def receive_turn_data(self):
+		pass
