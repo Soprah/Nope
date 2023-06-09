06714a5760091c48955c746cce6a1fd0cac9bc66
Eric <soprah@gmx.de>
2023-06-09 02:13:05 +0200

    Class GameManagement: Added tests for set_room()

--- src/gamemanagement/GameManagement.py
+++ src/gamemanagement/GameManagement.py
@@ -7,31 +7,10 @@ class GameManagement:
     HINWEIS:    Diese Klasse verwaltet alle Spielsitzungen
 
                 Grundsätzlicher Ablauf:
-                ⇾ GM bekommt Daten
-                	* session_id
-                	* Funktion (Request), die ausgeführt werden soll
+                ⇾ GM bekommt Daten.
                 ⇾ GM sucht passendes Spiel für die empfangenen Daten.
-                ⇾ GM weist die empfangenen Daten der gesuchten Spielsitzung zu
+                ⇾ GM weist die empfangenen Daten der gesuchten Spielsitzung zu.
     """
-	"""
-	def __init__(self):
-		self.game_sessions = {}
-		self.game_
-
-	def assign_player_to_game(self, p1_id, p1_name, p2_id, p2_name):
-		if p1_id in self.game_sessions or p2_id in self.game_sessions or p1_id == p2_id:
-			raise ValueError("Duplicate player ID!")
-
-		p1 = Player(p1_name, p1_id)
-		p2 = Player(p2_name, p2_id)
-		game = Game(p1, p2)
-		self.game_sessions[p1_id] = game
-		self.game_sessions[p2_id] = game
-
-	def get_game(self, p_id):
-		game = self.game_sessions.get(p_id)
-		return game
-	"""
 
 	_instance = None
 	def __init__(self):
@@ -61,7 +40,7 @@ class GameManagement:
 			player_1 = room.get("player_1")
 			player_2 = Player(player_name, player_id)
 			game = Game(player_1, player_2)
-			self.add_game_to_sessions(game, player_1.id, player_2)
+			self.add_game_to_sessions(game, player_1.id, player_2.id)
 			room["game"] = game
 			room["player_2"] = player_2
 		# Room existiert nicht
@@ -77,7 +56,7 @@ class GameManagement:
 		return self.rooms[room_id]
 
 	def does_room_exist(self, room_id):
-		return self.get_room(room_id) is not None
+		return room_id in self.rooms
 
 	def get_game(self, p_id):
 		game = self.sessions.get(p_id)
--- test/gamemanagement/test_GameManagement_set_room.py
+++ test/gamemanagement/test_GameManagement_set_room.py
@@ -1,17 +1,68 @@
 import unittest
 
+from src.gamemanagement.GameManagement import GameManagement
+
 
 class TestGameManagementSetRoom(unittest.TestCase):
-    def setUp(self):
-        self.player_1_name = "Rainer"
-        self.player_2_name = "Sabine"
 
+    def setUp(self):
+        self.gm = GameManagement.get_instance(self=GameManagement)
+
+# Soll-Funktion
+
+    # Raum leer ⇾ Erstelle Room mit Player
+    def test_set_room_no_room_with_id(self):
+        player_data = {
+            "room": "r1",
+            "token": 24,
+            "name": "rainer"
+        }
+
+        self.gm.set_room(player_data)
+
+        # 'rooms' besitzt nur einen room
+        self.assertTrue(len(self.gm.rooms) == 1)
+
+        # room besitzt richtige daten
+        room = self.gm.get_room("r1")
+        self.assertEqual(room.get("player_1").name, "rainer")
+        self.assertIsNone(room.get("player_2"))
+        self.assertIsNone(room.get("game"))
+
+    # Raum halbvoll ⇾ Fülle Room mit Player
+    def test_set_room_existing_room_with_id(self):
+        player_1_data = {
+            "room": "r1",
+            "token": 24,
+            "name": "rainer"
+        }
+        player_2_data = {
+            "room": "r1",
+            "token": 61,
+            "name": "sabine"
+        }
+        self.gm.set_room(player_1_data)
+        self.gm.set_room(player_2_data)
+
+        # 'rooms' besitzt zwei rooms
+        self.assertTrue(len(self.gm.rooms) == 1)
+
+        # room besitzt richtige daten
+        room = self.gm.get_room("r1")
+        self.assertEqual(room.get("player_1").name, "rainer")
+        self.assertEqual(room.get("player_2").name, "sabine")
+        self.assertIsNotNone(room.get("game"))
+
+        # game richtig in sessions gespeichert
+        game_1 = self.gm.get_game(24)
+        game_2 = self.gm.get_game(61)
+        self.assertEqual(game_1, game_2)
 
 
 # Fehlerquellen
-    # Spieler ist schon Teil des Room
-    # Spieler ist schon Teil eines laufenden Spiels
-    # Der Room ist voll
+# Spieler ist schon Teil des Room
+# Spieler ist schon Teil eines laufenden Spiels
+# Der Room ist voll
 
 if __name__ == '__main__':
     unittest.main()
